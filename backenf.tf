7173e3c7a943931008f5fab811e8017ef43b60e7
mounika <118247461+mounikamona278@users.noreply.github.com>
2026-07-23 19:10:27 +0530

    backenf.tf

--- backenf.tf
+++ backenf.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "rg"  
+    resource_group_name  = "tfstate_rg"  
     storage_account_name = "st_acc"                      
-    container_name       = "con"                       
+    container_name       = "tfstate_con"                       
     key                  = "dev.terraform.tfstate"        
   }
   }
